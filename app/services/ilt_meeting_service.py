c9dbcf39ec0f62eb7ef3514ff9a46e365b5dce1a
ashutosh-sortinghat <127046356+ashutosh-sortinghat@users.noreply.github.com>
2023-07-10 00:29:47 +0530

    code clean and correct the date format as previouly

--- app/services/ilt_meeting_service.py
+++ app/services/ilt_meeting_service.py
@@ -51,11 +51,11 @@ class IltMeetingService:
                     ilt_list.append({
                                     "iltId": ilt_id,
                                     "iltMeetingId": meeting_record.id,
-                                    "scheduledStartDate":(meeting_record.schedule_start_at.strftime("%Y-%m-%d %I:%M%p") 
+                                    "scheduledStartDate":(meeting_record.schedule_start_at.strftime("%Y-%m-%d %H:%M") 
                                     if meeting_record.schedule_start_at else meeting_record.schedule_start_at),
-                                    "meetingStart": (meeting_record.start_at.strftime("%Y-%m-%d %I:%M%p") 
+                                    "meetingStart": (meeting_record.start_at.strftime("%Y-%m-%d %H:%M") 
                                     if meeting_record.start_at else meeting_record.start_at),
-                                    "meetingEnd": (meeting_record.end_at.strftime("%Y-%m-%d %I:%M%p") 
+                                    "meetingEnd": (meeting_record.end_at.strftime("%Y-%m-%d %H:%M") 
                                     if meeting_record.end_at else meeting_record.end_at),
                                     "location": meeting_record.location,
                                     "meetingStatus": status
@@ -94,11 +94,11 @@ class IltMeetingService:
                 val = {
                     "iltId": ilt_id,
                     "iltMeetingId": mid,
-                    "scheduledStartDate": (meeting_record.schedule_start_at.strftime("%Y-%m-%d  %I:%M%p") 
+                    "scheduledStartDate": (meeting_record.schedule_start_at.strftime("%Y-%m-%d  %H:%M") 
                                     if meeting_record.schedule_start_at else meeting_record.schedule_start_at),
-                    "meetingStart": (meeting_record.start_at.strftime("%Y-%m-%d %I:%M%p") 
+                    "meetingStart": (meeting_record.start_at.strftime("%Y-%m-%d %H:%M") 
                                     if meeting_record.start_at else meeting_record.start_at),
-                    "meetingEnd": (meeting_record.end_at.strftime("%Y-%m-%d %I:%M%p") 
+                    "meetingEnd": (meeting_record.end_at.strftime("%Y-%m-%d %H:%M") 
                                     if meeting_record.end_at else meeting_record.end_at),
                     "location": meeting_record.location,
                     "meetingStatus": status
@@ -193,126 +193,124 @@ class IltMeetingService:
             raise CustomException(404,  "meeting records not found")
 
     def get_meeting_info(self, User_id: int, iltId: int, meeting_id: int,  db: Session):
-        try:
-            user = db.query(MdlUsers).filter(
-                MdlUsers.id == User_id).one_or_none()
-            if user is None:
-                raise CustomException(404,  "User_id not found")
-            ilt_record = db.query(MdlIlts).filter(
-                MdlIlts.id == iltId).one_or_none()
-            if ilt_record is None:
-                raise CustomException(404,  "ilt_id not found")
-            ilt_meeting_record = db.query(MdlMeetings).filter(
-                MdlMeetings.id == meeting_id).one_or_none()
-            if ilt_meeting_record is None:
-                raise CustomException(404,  "ilt_meeting records not found")
-            db_ilt_meeting_record = (db.query(MdlIltMeetings)
-                                     .filter(MdlIltMeetings.ilt_id == iltId,
-                                             MdlIltMeetings.ilt_meeting_id == meeting_id)
-                                     .one_or_none())
-            if db_ilt_meeting_record is None:
+        
+        user = db.query(MdlUsers).filter(
+            MdlUsers.id == User_id).one_or_none()
+        if user is None:
+            raise CustomException(404,  "User_id not found")
+        ilt_record = db.query(MdlIlts).filter(
+            MdlIlts.id == iltId).one_or_none()
+        if ilt_record is None:
+            raise CustomException(404,  "ilt_id not found")
+        ilt_meeting_record = db.query(MdlMeetings).filter(
+            MdlMeetings.id == meeting_id).one_or_none()
+        if ilt_meeting_record is None:
+            raise CustomException(404,  "ilt_meeting records not found")
+        db_ilt_meeting_record = (db.query(MdlIltMeetings)
+                                    .filter(MdlIltMeetings.ilt_id == iltId,
+                                            MdlIltMeetings.ilt_meeting_id == meeting_id)
+                                    .one_or_none())
+        if db_ilt_meeting_record is None:
+            raise CustomException(
+                404,  "Meeting ID is not associated with ILT id")
+        if ilt_record.owner_id == User_id:
+            ilt_members_ids = [x.member_id for x in db.query(
+                MdlIltMembers).filter(MdlIltMembers.ilt_id == iltId).all()]
+        else:
+            check_ilt_user_map_record = (db.query(MdlIltMembers)
+                                            .filter(MdlIltMembers.ilt_id == iltId, MdlIltMembers.member_id == User_id)
+                                            .one_or_none())
+            if check_ilt_user_map_record is None:
                 raise CustomException(
-                    404,  "Meeting ID is not associated with ILT id")
-            if ilt_record.owner_id == User_id:
-                ilt_members_ids = [x.member_id for x in db.query(
-                    MdlIltMembers).filter(MdlIltMembers.ilt_id == iltId).all()]
+                    404,  "User ID is not associated with ILT")
+            ilt_members_ids = [User_id]
+
+        members_Info_dict = []
+        meeting_response_id = 0
+
+        for uid in ilt_members_ids:
+            user_record = db.query(MdlUsers).filter(
+                MdlUsers.id == uid).one()
+            meeting_response_row = db.query(MdlIltMeetingResponses)\
+                .filter(MdlIltMeetingResponses.meeting_id == meeting_id,
+                        MdlIltMeetingResponses.meeting_user_id == uid).one_or_none()
+
+            if meeting_response_row is None:
+                continue
             else:
-                check_ilt_user_map_record = (db.query(MdlIltMembers)
-                                             .filter(MdlIltMembers.ilt_id == iltId, MdlIltMembers.member_id == User_id)
-                                             .one_or_none())
-                if check_ilt_user_map_record is None:
-                    raise CustomException(
-                        404,  "User ID is not associated with ILT")
-                ilt_members_ids = [User_id]
+                meeting_response_id = meeting_response_row.meeting_response_id
 
-            members_Info_dict = []
-            meeting_response_id = 0
+            meeting_response_record = db.query(MdlMeetingsResponse)\
+                .filter(MdlMeetingsResponse.id == meeting_response_id).one()
 
-            for uid in ilt_members_ids:
-                user_record = db.query(MdlUsers).filter(
-                    MdlUsers.id == uid).one()
-                meeting_response_row = db.query(MdlIltMeetingResponses)\
-                    .filter(MdlIltMeetingResponses.meeting_id == meeting_id,
-                            MdlIltMeetingResponses.meeting_user_id == uid).one_or_none()
+            update_records = db.query(Mdl_updates)\
+                .filter(Mdl_updates.meeting_response_id == meeting_response_id)\
+                .all()
+            user_update_record = [
+                {
+                    "updateId": record.id,
+                    "description": record.description
+                }
+                for record in update_records
+            ] if update_records else []
+            todo_task_records = db.query(MdlIlt_ToDoTask)\
+                .filter(MdlIlt_ToDoTask.meeting_response_id == meeting_response_id)\
+                .all()
+            user_todolist_record = [
+                {
+                    "todoListId": record.id,
+                    "description": record.description,
+                    "dueDate": record.due_date,
+                    "status": record.status
+                }
+                for record in todo_task_records
+            ] if todo_task_records else []
 
-                if meeting_response_row is None:
-                    continue
-                else:
-                    meeting_response_id = meeting_response_row.meeting_response_id
+            issue_record = db.query(MdlIltissue)\
+                .filter(MdlIltissue.meeting_response_id == meeting_response_id).order_by(MdlIltissue.id.desc()).all()
 
-                meeting_response_record = db.query(MdlMeetingsResponse)\
-                    .filter(MdlMeetingsResponse.id == meeting_response_id).one()
+            user_issues_record = [db.query(Mdl_issue)
+                                    .filter(Mdl_issue.id == record.id).one_or_none() for record in issue_record]  \
+                if issue_record else []
 
-                update_records = db.query(Mdl_updates)\
-                    .filter(Mdl_updates.meeting_response_id == meeting_response_id)\
-                    .all()
-                user_update_record = [
-                    {
-                        "updateId": record.id,
-                        "description": record.description
-                    }
-                    for record in update_records
-                ] if update_records else []
-                todo_task_records = db.query(MdlIlt_ToDoTask)\
-                    .filter(MdlIlt_ToDoTask.meeting_response_id == meeting_response_id)\
-                    .all()
-                user_todolist_record = [
-                    {
-                        "todoListId": record.id,
-                        "description": record.description,
-                        "dueDate": record.due_date.strftime("%Y-%m-%d %I:%M%p"),
-                        "status": record.status
-                    }
-                    for record in todo_task_records
-                ] if todo_task_records else []
+            members_Info_dict.append(
+                {
+                    "iltMeetingResponseId": meeting_response_id,
+                    "iltMeetingId": meeting_id,
+                    "member": {
+                        "userId": user_record.id,
+                        "firstName": user_record.fname,
+                        "lastName": user_record.lname,
+                        "emailId": user_record.email
+                    },
+                    "attendance": meeting_response_record.attendance_flag,
+                    "personalBest": meeting_response_record.checkin_personal_best,
+                    "professionalBest": meeting_response_record.checkin_professional_best,
+                    "rating": meeting_response_record.rating,
+                    "feedback": meeting_response_record.feedback,
+                    "notes": meeting_response_record.notes,
+                    "rockName": meeting_response_record.rockName,
+                    "onTrack": meeting_response_record.onTrack,
+                    "updates": user_update_record,
+                    "todoList": user_todolist_record,
+                    "issues":
+                    [{
+                        "issueId": user_issues_single_record.id,
+                        "issue": user_issues_single_record.issue,
+                        "priorityId": user_issues_single_record.priority,
+                        "date": user_issues_single_record.created_at,
+                        "resolvedFlag": user_issues_single_record.resolves_flag,
+                        "recognizePerformanceFlag": user_issues_single_record.recognize_performance_flag,
+                        "teacherSupportFlag": user_issues_single_record.teacher_support_flag,
+                        "leaderSupportFlag": user_issues_single_record.leader_support_flag,
+                        "advanceEqualityFlag": user_issues_single_record.advance_equality_flag,
+                        "othersFlag": user_issues_single_record.others_flag
+                    } for user_issues_single_record in user_issues_record]
+                    if user_issues_record else []
+                }
+            )
 
-                issue_record = db.query(MdlIltissue)\
-                    .filter(MdlIltissue.meeting_response_id == meeting_response_id).order_by(MdlIltissue.id.desc()).all()
-
-                user_issues_record = [db.query(Mdl_issue)
-                                      .filter(Mdl_issue.id == record.id).one_or_none() for record in issue_record]  \
-                    if issue_record else []
-
-                members_Info_dict.append(
-                    {
-                        "iltMeetingResponseId": meeting_response_id,
-                        "iltMeetingId": meeting_id,
-                        "member": {
-                            "userId": user_record.id,
-                            "firstName": user_record.fname,
-                            "lastName": user_record.lname,
-                            "emailId": user_record.email
-                        },
-                        "attendance": meeting_response_record.attendance_flag,
-                        "personalBest": meeting_response_record.checkin_personal_best,
-                        "professionalBest": meeting_response_record.checkin_professional_best,
-                        "rating": meeting_response_record.rating,
-                        "feedback": meeting_response_record.feedback,
-                        "notes": meeting_response_record.notes,
-                        "rockName": meeting_response_record.rockName,
-                        "onTrack": meeting_response_record.onTrack,
-                        "updates": user_update_record,
-                        "todoList": user_todolist_record,
-                        "issues":
-                        [{
-                            "issueId": user_issues_single_record.id,
-                            "issue": user_issues_single_record.issue,
-                            "priorityId": user_issues_single_record.priority,
-                            "date": user_issues_single_record.created_at.strftime("%Y-%m-%d %I:%M%p"),
-                            "resolvedFlag": user_issues_single_record.resolves_flag,
-                            "recognizePerformanceFlag": user_issues_single_record.recognize_performance_flag,
-                            "teacherSupportFlag": user_issues_single_record.teacher_support_flag,
-                            "leaderSupportFlag": user_issues_single_record.leader_support_flag,
-                            "advanceEqualityFlag": user_issues_single_record.advance_equality_flag,
-                            "othersFlag": user_issues_single_record.others_flag
-                        } for user_issues_single_record in user_issues_record]
-                        if user_issues_record else []
-                    }
-                )
-
-            return members_Info_dict
-        except Exception as e:
-            raise CustomException(500,  f"Internal server error {str(e)}")
+        return members_Info_dict
 
     def start_ilt_meeting(self, UserId: int, meeting_id: int, ilt_id: int, db: Session):
 
