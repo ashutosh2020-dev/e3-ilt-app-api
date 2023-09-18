4b8084440e36e3b938353b7dbb19d0e9f468fc46
ashutosh-sortinghat <127046356+ashutosh-sortinghat@users.noreply.github.com>
2023-09-18 21:13:21 +0530

    added dashboard related api

--- app/services/dashboard_service.py
+++ app/services/dashboard_service.py
@@ -211,6 +211,189 @@ class DashboardService:
             "meetingsObj": list_of_meeting_obj
         }
 
+    def get_ilt_Meeting_dashboard_info(self, user_id: int, meetingId:int, ilt_id: int, db: Session):
+        # check user
+        user_record = db.query(MdlUsers).filter(
+            MdlUsers.id == user_id).one_or_none()
+        if user_record is None:
+            raise CustomException(404,  "User not found")
+        meeting_reco = db.query(MdlIltMeetings).filter(
+                MdlIltMeetings.ilt_id == ilt_id, MdlIltMeetings.ilt_meeting_id== meetingId).one_or_none()
+        if meeting_reco is None:
+            raise CustomException(404,  "meeting id is not associated with iltId ")
+        if user_record.role_id > 4:
+            raise CustomException(404,  "You don't have permission, Please contact administrative.")
+        
+        # num_of_ended_meeting = 0
+        # num_of_notStarted_meeting = 0
+        # num_of_inprogress_meeting = 0
+        num_of_member_in_ilt = 0
+        list_of_ended_meeting_ids = []
+        list_of_meeting_obj = []
+        
+        meeting_records = (db.query(MdlMeetings)
+                                .join(MdlIltMeetings, MdlMeetings.id == MdlIltMeetings.ilt_meeting_id)
+                                .filter(MdlIltMeetings.ilt_id == ilt_id, MdlIltMeetings.ilt_meeting_id== meetingId)
+                                .order_by(MdlMeetings.schedule_start_at.asc())
+                                .all()
+                                )
+        if not meeting_records:
+            raise CustomException(404,  "No records found.")
+        
+        for meeting_record in meeting_records:
+            status = calculate_meeting_status(
+                schedule_start_at=meeting_record.schedule_start_at,
+                start_at=meeting_record.start_at if meeting_record.start_at else 0,
+                end_at=meeting_record.end_at if meeting_record.end_at else 0)
+            if status == 2:
+                list_of_ended_meeting_ids.append(meeting_record.id)
+            else:
+                raise CustomException(404,  "Please make sure your Meeting is not ended")
+
+        members_list = [map_record.member_id
+                        for map_record in db.query(MdlIltMembers)
+                        .filter(MdlIltMembers.ilt_id == ilt_id)
+                        .all()]
+        num_of_member_in_ilt = len(members_list)
+
+        for mid in list_of_ended_meeting_ids:
+
+            member_meeting_response_id_list = [map_record.meeting_response_id
+                                               for map_record in db.query(MdlIltMeetingResponses)
+                                               .filter(MdlIltMeetingResponses.meeting_id == mid)
+                                               .all()
+                                               ]
+
+            member_meeting_responce_records = [db.query(MdlMeetingsResponse)
+                                               .filter(MdlMeetingsResponse.id == m_r_id).one()
+                                               for m_r_id in member_meeting_response_id_list]
+        #  meeting end time
+            meetingEndTime = db.query(MdlMeetings).get(mid).end_at 
+        # attandence
+            attandence_nominator = sum([record.attendance_flag
+                                        for record in member_meeting_responce_records])
+
+            attandence_denominator = num_of_member_in_ilt
+            avg_attendence = {
+                        "percentage": attandence_nominator*100/attandence_denominator,
+                        "total": attandence_denominator
+                    }
+
+        # rating :
+            mid_ratings = [
+                record.rating for record in member_meeting_responce_records if record.rating]
+            avg_rating = 0
+            if mid_ratings:
+                rating_nominator = sum(mid_ratings)
+                rating_denominator = len(mid_ratings)*5
+                avg_rating = {
+                        "percentage": (rating_nominator/rating_denominator)*100,
+                        "total": rating_denominator
+                    }
+
+        # rock_on_track
+            mid_rocks = [
+                record.onTrack for record in member_meeting_responce_records if record.rockName]
+            avg_rock = 0
+            if mid_rocks:
+                rock_nominator = sum(mid_rocks)
+                rock_denominator = len(mid_rocks)
+                avg_rock = {
+                        "percentage": (rock_nominator/rock_denominator)*100,
+                        "total": rock_denominator
+                    }
+
+        # issue
+            list_of_list_of_issue_records = [db.query(MdlIltissue)
+                                             .filter(MdlIltissue.meeting_response_id == m_r_id)
+                                             .all()
+                                             for m_r_id in member_meeting_response_id_list]
+            issue_id_list = []
+            avg_issueResolve = []
+            avg_issueObj={}
+            if list_of_list_of_issue_records:
+                for record in list_of_list_of_issue_records:
+                    if record:
+                        for r in record:
+                            issue_id_list.append(r.issue_id)
+                    else:
+                        pass
+            denominator = 0
+            numOfIssueRepeat=0
+            issue_nominators = {
+                    'resolve': 0,
+                    'recognizePerformance': 0,
+                    'teacherSupport': 0,
+                    'leaderSupport': 0,
+                    'advanceEquality': 0,
+                    'othersFlag': 0
+                }
+            if issue_id_list:
+                meetings_issue_resolve_list = [db.query(Mdl_issue)
+                                               .get(issue_id)
+                                               .resolves_flag
+                                               for issue_id in issue_id_list]
+                for issue_id in issue_id_list:
+                    numOfIssueRepeat += len([record.meeting_response_id for record in db.query(MdlIltissue).filter(MdlIltissue.issue_id==issue_id).all()])
+                    issue_re = db.query(Mdl_issue).get(issue_id)
+                    issue_nominators['resolve'] += int(issue_re.resolves_flag)
+                    issue_nominators['recognizePerformance'] += int(issue_re.recognize_performance_flag)
+                    issue_nominators['teacherSupport'] += int(issue_re.teacher_support_flag)
+                    issue_nominators['leaderSupport'] += int(issue_re.leader_support_flag)
+                    issue_nominators['advanceEquality'] += int(issue_re.advance_equality_flag)
+                    issue_nominators['othersFlag'] += int(issue_re.others_flag)
+                    denominator += 1
+            avg_issueObj = {flag: {'percentage':(issue_nominators[flag]/denominator)*100 if denominator > 0 else 0, 'total':denominator} 
+                                            for flag in issue_nominators 
+                            }
+            if numOfIssueRepeat>0:
+                avg_issueObj["avgIssueRepeat"] =  {"percentage":(numOfIssueRepeat/denominator)*100, "total":denominator}
+            else:
+                avg_issueObj["avgIssueRepeat"] =  {"percentage":0 if denominator==0 else 0, "total":denominator} 
+            avg_issueObj["totalIssues"] = denominator
+
+            # To-Do
+            list_list_of_toDo_records = (db.query(MdlIlt_ToDoTask)
+                                         .filter(MdlIlt_ToDoTask.meeting_response_id == m_r_id)
+                                         .all()
+                                         for m_r_id in member_meeting_response_id_list)
+            meeting_todo_record_list = []
+            avg_ToDo = {}
+            ToDo_denominator = 0
+            if list_list_of_toDo_records:
+                for list_todo_record in list_list_of_toDo_records:
+                    for todo_record in list_todo_record:
+                        meeting_todo_record_list.append(todo_record.status)
+
+            if meeting_todo_record_list:
+                ToDo_nominator = sum(meeting_todo_record_list)
+                ToDo_denominator = len(meeting_todo_record_list)
+                avg_ToDo = {
+                        "percentage": ToDo_nominator/ToDo_denominator,
+                        "total": ToDo_denominator
+                    }
+
+            list_of_meeting_obj.append(
+                {
+                    "id": ilt_id,
+                    "meetingId":mid,
+                    "meetingEndDate":meetingEndTime,
+                    "attendancePercentage": avg_attendence,
+                    "rockOnTrack": avg_rock,
+                    "avgRatings": avg_rating,
+                    "avgtoDo": avg_ToDo,
+                    "issues": avg_issueObj 
+                })
+
+        return {
+            "id": ilt_id, 
+            "numOfMembersInIlt": num_of_member_in_ilt,
+            "meetingsObj": list_of_meeting_obj
+        }
+
+
+
+
     def get_dashboard_info(self, user_id: int, ilt_id: int, db: Session):
         # check user
         user_record = db.query(MdlUsers).filter(
