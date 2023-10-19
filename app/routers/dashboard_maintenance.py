a0fffdee3f7ec91347ba23de56795fc360eb02da
ashutosh-sortinghat <127046356+ashutosh-sortinghat@users.noreply.github.com>
2023-10-19 16:35:26 +0530

    added  TimeFilterParameter to dashboard for ILT

--- app/routers/dashboard_maintenance.py
+++ app/routers/dashboard_maintenance.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter, Depends, Query, Header
 from sqlalchemy.orm import Session
 from app.config.database import get_db
 from app.services.dashboard_service import DashboardService
-from app.schemas.dashboard_schemas import DashboardFilterParamaters 
+from app.schemas.dashboard_schemas import DashboardFilterParamaters, TimeFilterParameter
 router = APIRouter()
 Dashboard_Service = DashboardService()
 
-@router.get("/api/v1/dashboard/ilt/{iltId}")
-async def Show_Ilt_Dashboard( iltId :int, UserId: int=Header(convert_underscores=False), db: Session = Depends(get_db)):
-    return Dashboard_Service.get_ilt_Meetings_dashboard_info(user_id=UserId, ilt_id=iltId, db=db)
+@router.post("/api/v1/dashboard/ilt/{iltId}")
+async def Show_Ilt_Dashboard( iltId :int, FilterParamaters:TimeFilterParameter, UserId: int=Header(convert_underscores=False), db: Session = Depends(get_db)):
+    return Dashboard_Service.get_ilt_Meetings_dashboard_info(user_id=UserId, 
+                                                             FilterParamaters=FilterParamaters , ilt_id=iltId, db=db)
 
 @router.get("/api/v1/dashboard/ilt/{iltId}/meeting/{meetingId}")
 async def Show_Ilt_Dashboard_wrt_meeting( iltId :int, meetingId:int, UserId: int=Header(convert_underscores=False), db: Session = Depends(get_db)):
