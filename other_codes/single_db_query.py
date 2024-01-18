89cde68d5893ec6fe5da81087c8bb7ae3c0d4142
ashutosh-sortinghat <127046356+ashutosh-sortinghat@users.noreply.github.com>
2024-01-18 18:44:58 +0530

    rename file and some changes in existing wrt exporting db data

--- other_codes/single_db_query.py
+++ other_codes/single_db_query.py
@@ -1,13 +1,31 @@
-from sqlalchemy import create_engine, text
 import pandas as pd
+from sqlalchemy import create_engine, text
 
-DATABASE_URL = "mysql://sortinghatadmin:Sortinghat#Middle123@rds-middle-ilt-app.c61momvubcsz.us-east-1.rds.amazonaws.com:3306/dbiltappuat"
+fromdb = "UAT"  # "PROD"
+todb = "sqllite"
+instance = "dbiltappuat"
 
-# engine = create_engine(DATABASE_URL)
-# with engine.connect() as conn:
-#     user_cset_record = conn.execute(text(f"SELECT * FROM roles;"))
-#     for re in user_cset_record:
-#         print(type(re))
-#         break
-df = pd.read_sql_table('roles', DATABASE_URL)
-print(df.shape)
+
+
+def save_db_info(fromdb, todb, instanceName):
+
+    DATABASE_URLS_DICT = {
+            "PROD": "mysql://sortinghatadmin:Sortinghat#Middle123@rds-middle-ilt-app.c61momvubcsz.us-east-1.rds.amazonaws.com:3306/dbiltapp",
+            "UAT": "mysql://sortinghatadmin:Sortinghat#Middle123@rds-middle-ilt-app.c61momvubcsz.us-east-1.rds.amazonaws.com:3306/dbiltappuat",
+            "sqllite": "sqlite:///ilt_db.db"
+    }
+    DATABASE_URL = DATABASE_URLS_DICT.get(fromdb)
+    DATABASE_URL2 = DATABASE_URLS_DICT.get(todb)
+
+    conn = create_engine(DATABASE_URL).connect()
+    conn2 = create_engine(DATABASE_URL2).connect()
+    db_schema = pd.read_sql('SELECT * FROM INFORMATION_SCHEMA.COLUMNS;', conn)
+
+
+    list_of_table_name = db_schema[db_schema["TABLE_SCHEMA"]== instanceName]["TABLE_NAME"].unique()
+    for tname in list_of_table_name:
+        df = pd.read_sql_table(tname, conn)
+        df.to_sql(tname, conn2, if_exists='replace')
+    print("have successfully exported!")
+ 
+save_db_info(fromdb =fromdb , todb = todb, instanceName = instance)
